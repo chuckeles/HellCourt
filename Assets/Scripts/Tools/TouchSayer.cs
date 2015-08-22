0469d7312d773295756bba9cad2b92f322e97878
Martin Chuckeles <martin@jakubik.info>
2015-08-22 18:00:42 +0200

    Prefabized everything!

---
+++ Assets/Scripts/Tools/TouchSayer.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+/// <summary>
+///   Says something on trigger and disappears.
+/// </summary>
+[RequireComponent(typeof (Collider2D))]
+public class TouchSayer : MonoBehaviour {
+
+  public void OnTriggerEnter2D(Collider2D collision) {
+    // get manager
+    var manager = GameObject.Find("DialogManager").GetComponent<DialogManager>();
+
+    // say a thing
+    manager.Say(Sentences[Random.Range(0, Sentences.Length)],
+                Attach ? new Vector2(0, 20f) : (Vector2) collision.transform.position + new Vector2(0, 20f),
+                SayTime,
+                Attach ? collision.gameObject : null);
+
+    // disappear
+    Destroy(gameObject);
+  }
+
+  /// <summary>
+  ///   Whether to attach the saying to collider.
+  /// </summary>
+  public bool Attach = true;
+
+  /// <summary>
+  ///   How long to talk.
+  /// </summary>
+  public float SayTime = 2f;
+
+  /// <summary>
+  ///   What to say.
+  /// </summary>
+  public string[] Sentences;
+
+}
