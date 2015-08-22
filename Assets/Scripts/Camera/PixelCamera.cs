b502f6316ddb527b1163ccbfd035dd3d7135ef2d
Martin Chuckeles <martin@jakubik.info>
2015-08-22 06:05:12 +0200

    Added PixelCamera script

---
+++ Assets/Scripts/Camera/PixelCamera.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+/// <summary>
+///   Sets the camera's size for a pixel game.
+/// </summary>
+[RequireComponent(typeof (Camera)), ExecuteInEditMode]
+public class PixelCamera : MonoBehaviour {
+
+  public void Start() {
+    // get the camera
+    var camera = GetComponent<Camera>();
+
+    // calculate scale
+    var scale = Screen.height / InternalHeight;
+
+    // set orthographic and size
+    camera.orthographic = true;
+    camera.orthographicSize = (Screen.height / 2f) / (PixelsPerUnit * scale);
+  }
+
+  /// <summary>
+  ///   Game's internal screen height.
+  /// </summary>
+  public int InternalHeight = 160;
+
+  /// <summary>
+  ///   Pixels to unit ratio.
+  /// </summary>
+  public int PixelsPerUnit = 1;
+
+}
