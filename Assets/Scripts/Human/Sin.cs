e1314a018bc2839cd046488238cf7a202f84abca
Martin Chuckeles <martin@jakubik.info>
2015-08-22 10:33:42 +0200

    Added sin struct

---
+++ Assets/Scripts/Human/Sin.cs
@@ -0,0 +1,21 @@
+﻿/// <summary>
+///   Defines a sin.
+/// </summary>
+public struct Sin {
+
+  /// <summary>
+  ///   How much mental pain is needed for the sin.
+  /// </summary>
+  public float MentalPainNeeded;
+
+  /// <summary>
+  ///   How much physical pain is needed for the sin.
+  /// </summary>
+  public float PhysicalPainNeeded;
+
+  /// <summary>
+  ///   What do humans say.
+  /// </summary>
+  public string SayLine;
+
+}
