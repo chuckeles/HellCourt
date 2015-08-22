84fb04effd9416d894f372c94cde7aa5cd6c011c
Martin Chuckeles <martin@jakubik.info>
2015-08-22 10:19:59 +0200

    Added tutorial destroyers

---
+++ Assets/Scripts/Tools/TouchDestroyer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+/// <summary>
+///   Destroys targets on trigger.
+/// </summary>
+[RequireComponent(typeof (Collider2D))]
+public class TouchDestroyer : MonoBehaviour {
+
+  public void OnTriggerEnter2D(Collider2D collision) {
+    if (collision.tag == Tag) {
+      // destroy targets
+      foreach (var target in Targets) {
+        Destroy(target);
+      }
+
+      // destroy myself, goodbye
+      Destroy(gameObject);
+    }
+  }
+
+  /// <summary>
+  ///   Collider's tag.
+  /// </summary>
+  public string Tag;
+
+  /// <summary>
+  ///   Game objects to destroy.
+  /// </summary>
+  public GameObject[] Targets;
+
+}
