30c8c9f738f394d06c3f1418cf7ed1a8ee361ae2
Martin Chuckeles <martin@jakubik.info>
2015-08-22 06:47:42 +0200

    Fixed an issue with getting stuck in walls

--- Assets/Scripts/Devil/Movement.cs
+++ Assets/Scripts/Devil/Movement.cs
@@ -19,13 +19,29 @@ public class Movement : MonoBehaviour {
     // create new velocity
     var newVelocity = _body.velocity;
 
-    // set horizontal movement
-    newVelocity.x = moveInput * Speed;
+    // if we are moving horizontally
+    if (Mathf.Abs(moveInput) > 0) {
+      var side = Mathf.Sign(moveInput);
+
+      // test horizontal collision
+      var origin = (Vector2) transform.position + new Vector2(side * 8, 8);
+      var direction = side > 0 ? Vector2.right : Vector2.left;
+      var horizontalHit = Physics2D.Raycast(origin, direction, Speed * Time.fixedDeltaTime, SolidLayerMask);
+
+      if (!horizontalHit) {
+        // set horizontal movement
+        newVelocity.x = moveInput * Speed;
+      }
+    }
+    else {
+      // stop moving
+      newVelocity.x = 0;
+    }
 
     // test if on the ground
-    var hit = Physics2D.Raycast(transform.position, Vector2.down, 1f, SolidLayerMask);
+    var verticalHit = Physics2D.Raycast(transform.position, Vector2.down, 1f, SolidLayerMask);
 
-    if (hit && jumpInput) {
+    if (verticalHit && jumpInput) {
       // jump
       newVelocity.y = JumpStrength;
     }
