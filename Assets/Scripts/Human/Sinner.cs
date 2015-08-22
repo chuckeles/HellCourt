344cd9c297987cd3bcd314ff2ecc13ac2634bb97
Martin Chuckeles <martin@jakubik.info>
2015-08-22 10:36:38 +0200

    Added sinner script

---
+++ Assets/Scripts/Human/Sinner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+/// <summary>
+///   Manages sins and pain of a human.
+/// </summary>
+public class Sinner : MonoBehaviour {
+
+  /// <summary>
+  ///   Current mental pain.
+  /// </summary>
+  public float MentalPain = 0f;
+
+  /// <summary>
+  ///   Current physical pain.
+  /// </summary>
+  public float PhysicalPain = 0f;
+
+  /// <summary>
+  ///   Sins this human committed.
+  /// </summary>
+  public Sin[] Sins;
+
+}
