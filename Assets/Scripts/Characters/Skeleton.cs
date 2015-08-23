14c5eab543476fd78a81c932699331696ee4e84e
Martin Chuckeles <martin@jakubik.info>
2015-08-23 17:39:56 +0200

    Updated sentences for ghost

--- Assets/Scripts/Characters/Skeleton.cs
+++ Assets/Scripts/Characters/Skeleton.cs
@@ -85,7 +85,7 @@ public class Skeleton : MonoBehaviour {
     "Ou hello there!", "Bu bu bu bu!", "Don't be scared.", "What are you looking at?", "Yes, I'm alive.", "What?",
     "I'm made out of bones...", "Been here for so long...", "Ah! We have a company!", "Welcome.", "Don't waste my time.",
     "I'm in no hurry.", "I see.", "Can I play with them?", "What is this?", "Really?",
-    "I am really old.", "This will be fun", "Let's get to work."
+    "I am really old.", "This will be fun.", "Let's get to work."
   };
 
   /// <summary>
