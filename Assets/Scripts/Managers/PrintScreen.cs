2470c7ae4f873ec3572a42c2a670a2aec1519c2e
Martin Chuckeles <martin@jakubik.info>
2015-08-24 20:53:48 +0200

    Added printscreen input

--- Assets/Scripts/Managers/PrintScreen.cs
+++ Assets/Scripts/Managers/PrintScreen.cs
@@ -6,12 +6,10 @@
 public class PrintScreen : MonoBehaviour {
 
   public void Update() {
-    if (Input.GetKeyDown(KeyCode.Print)) {
-      Debug.Log("Print screen");
-
+    if (Input.GetButtonDown("Printscreen")) {
       var number = PlayerPrefs.GetInt("ScreenshotNumber", 0);
 
-      Application.CaptureScreenshot("Screenshot" + number);
+      Application.CaptureScreenshot("Screenshot" + number + ".png");
 
       PlayerPrefs.SetInt("ScreenshotNumber", number + 1);
     }
