d4e29ea593e84fa0b0e2aba1dbaf68981fea9e38
Martin Chuckeles <martin@jakubik.info>
2015-08-24 18:49:09 +0200

    Added fall damage

---
+++ Assets/Scripts/Characters/FallDamage.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+/// <summary>
+///   Hurts human on fall.
+/// </summary>
+[RequireComponent(typeof (Sinner))]
+public class FallDamage : MonoBehaviour {
+
+  public void Awake() {
+    // set things
+    _sinner = GetComponent<Sinner>();
+    _manager = FindObjectOfType<LevelManager>();
+  }
+
+  public void OnCollisionEnter2D(Collision2D collision) {
+    // calculate pain
+    var pain = collision.relativeVelocity.magnitude;
+    pain -= 200f;
+    pain /= 50f;
+    pain = Mathf.Clamp(pain, 0, float.MaxValue);
+
+    // apply pain
+    _sinner.PhysicalPain += pain * _manager.PainMultiplier;
+  }
+
+  /// <summary>
+  ///   The level manager.
+  /// </summary>
+  private LevelManager _manager;
+
+  /// <summary>
+  ///   Sinner component.
+  /// </summary>
+  private Sinner _sinner;
+
+}
