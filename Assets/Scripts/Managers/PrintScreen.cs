542108453e99dbc3d1dc6e0eee570f1b0daef303
Martin Chuckeles <martin@jakubik.info>
2015-08-24 20:37:11 +0200

    Added printscreen

---
+++ Assets/Scripts/Managers/PrintScreen.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Whatever.
+/// </summary>
+public class PrintScreen : MonoBehaviour {
+
+  public void Update() {
+    if (Input.GetKeyDown(KeyCode.Print)) {
+      var number = PlayerPrefs.GetInt("ScreenshotNumber", 0);
+
+      Application.CaptureScreenshot("Screenshot" + number);
+
+      PlayerPrefs.SetInt("ScreenshotNumber", number + 1);
+    }
+  }
+
+}
